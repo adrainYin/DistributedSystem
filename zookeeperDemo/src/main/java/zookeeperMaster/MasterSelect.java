216e80807d940d23248a0aca0a740da6829b09a5
AdrainYin <ych19941103@163.com>
2019-04-09 20:22:06 +0800

    dubbo的demo

--- zookeeperDemo/src/main/java/zookeeperMaster/MasterSelect.java
+++ zookeeperDemo/src/main/java/zookeeperMaster/MasterSelect.java
@@ -10,6 +10,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 实现master选举的过程
+ */
 public class MasterSelect {
 
 
